68de021110d1f3be4f07b8983dcfc7e464e839e9
Henrique Quirino <piccocca@gmail.com>
2024-02-26 21:13:37 -0300

    :rocket: primeiro commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var x = 1
+var y = 2.1
+
+var dia = "segunda"
+
+console.log('O valor de x é: ' + x)
+console.log('O valor de y é: ' + y)
+console.log('Hoje é' + ' ' + dia)
+
+var z = x * y + dia
+
+console.log('O valor de z é: ' + z)
+
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z) 
+
+const pessoa = {
+
+    nome : 'Henrique',
+    profissao : 'Suporte de Redes N1',
+    idade : '21'
+
+}
+
+console.log(`Meu nome é ${pessoa.nome} trabalho como ${pessoa.profissao} e tenho ${pessoa.idade}.`)
+
+const aviao = {
+
+    modelo : 'Boeing' ,
+    capacidade : '600',
+    companhia : 'Qatar Airlines'    
+
+}
+
+console.log(`Este avião é um ${aviao.modelo} com capacidade para ${aviao.capacidade} pessoas da companhia ${aviao.companhia}.`)
+
